8944908de375208740340784cf62eb55fcaef87a
ezolla <ethanzoller111@gmail.com>
2019-03-06 21:07:47 -0800

    + ES Linting Settings

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "airbnb",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "react/jsx-filename-extension": 0
+  }
+};
